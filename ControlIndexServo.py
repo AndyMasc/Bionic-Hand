8ad8a86e4d2b1b48b4215b55dd6c367f51184104
AndyMasc <andy.mascarenhas12@gmail.com>
2025-05-27 10:29:10 +1200

    Fixed issue

--- ControlIndexServo.py
+++ ControlIndexServo.py
@@ -4,4 +4,3 @@ def controlIndex(joint1landmark, joint2landmark, unknownAngleLandmark):
     angle = getAngle(joint1landmark, joint2landmark, unknownAngleLandmark)
     angle = mapAngle(angle, 57,140,0,180)
     indexFinger.write(angle)
-
