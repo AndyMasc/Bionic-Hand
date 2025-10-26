54e36bfb05587b6e2d079ebb15d9d6aafd44a2f8
AndyMasc <andy.mascarenhas12@gmail.com>
2025-10-27 10:35:56 +1300

    Fix incorrect servo-pin relationship

--- ControlServo.py
+++ ControlServo.py
@@ -4,7 +4,7 @@ def controlServo(servo, landmark1, landmark2, landmark3):
     angle = getAngle(landmark1, landmark2, landmark3, 'yz')
     if servo == thumb: # Thumb moves in different plane compared to other fingers and requires different mapping values
         angle = getAngle(landmark1, landmark2, landmark3, 'xz') # Thumb moves in xz plane compared to yz plane for other fingers
-        angle = round(mapAngle(angle, 175, 330, 0, 90))
+        angle = round(mapAngle(angle, 175, 330, 0, 180))
     elif servo == pinkyFinger:
         angle = round(mapAngle(angle, 35, 176, 0, 180))
     else:
